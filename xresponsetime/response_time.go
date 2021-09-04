ddc9b5bfe05fff4bf631cd2f5a61f67df5602c5e
ringsaturn <ringsaturn.me@gmail.com>
2021-09-05 00:07:39 +0800

    fix time unit

--- xresponsetime/response_time.go
+++ xresponsetime/response_time.go
@@ -10,6 +10,6 @@ import (
 func Handler(c *gin.Context) {
 	startTime := time.Now()
 	c.Next()
-	duration := int(time.Since(startTime).Microseconds())
+	duration := int(time.Since(startTime).Milliseconds())
 	c.Header("x-response-time", strconv.Itoa(duration))
 }
